c89e40f210b5f55fd252fd95372b34556112e961
Massimo Marsiglia <marsigliamassi@gmail.com>
2024-06-19 00:48:54 +0200

    removed tests test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,16 @@
-package com.example.demo;
+// package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class) // Use Spring's JUnit support
-@SpringBootTest
-class DemoApplicationTests {
+// @ExtendWith(SpringExtension.class) // Use Spring's JUnit support
+// @SpringBootTest
+// class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
