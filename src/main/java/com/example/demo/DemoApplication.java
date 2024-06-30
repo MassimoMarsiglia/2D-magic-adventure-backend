52648dca65478e6ad5b6d3c0b1fc3702592a5fcf
Thi Thanh Hang Truong <thanhhang2x2@gmail.com>
2024-06-30 03:18:21 +0200

    map 2 done (change the map 1 and 2 for editing)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+// @SpringBootApplication
 // @PropertySource("classpath:.env") // needs to be commented out when pushed
 public class DemoApplication {
 
